e31ee85859c18d90c160db36e79cbb4ba4b35e5c
David Vaz <davidpvaz@protonmail.com>
2022-11-17 16:03:30 +0000

    updated deployment script

--- script/deploy_proxy.sh
+++ script/deploy_proxy.sh
@@ -3,4 +3,4 @@
 source .env
 
 
-forge create --rpc-url ${GOERLI_RPC_URL} --private-key ${PRIVATE_KEY} src/marketplace/Marketplace.sol:MarketplaceProxy --constructor-args 0x10043A9c6c3bd98083f721ec08067D39F1B041b7 1 --etherscan-api-key ${ETHERSCAN_API_KEY} --verify
+forge create --rpc-url ${GOERLI_RPC_URL} --private-key ${PRIVATE_KEY} src/marketplace/Marketplace.sol:MarketplaceProxy --constructor-args 0xC491903a27bDdBDECC9F4A29A486a2da84AF3326 100 --etherscan-api-key ${ETHERSCAN_API_KEY} --verify
